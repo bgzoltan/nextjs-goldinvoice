1225352ee967fd0f209b67448b8e0b8411f6389e
Bagdany Zoltan <bgzoltan@gmail.com>
2024-12-05 14:06:59 +1000

    partial rendering is added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {
+    ppr: "incremental",
+  },
 };
 
 export default nextConfig;

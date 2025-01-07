7ae616d0678347da1f8dcc5e006b141faaf18d93
Bagdany Zoltan <bgzoltan@gmail.com>
2025-01-07 17:26:13 +1000

    lusitana font replaced with suse

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,11 +1,6 @@
-import { Inter, Lusitana, SUSE } from "next/font/google";
+import { Inter, SUSE } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
-
-export const lusitana = Lusitana({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-});
 
 export const suse = SUSE({
   weight: ["400", "700"],
